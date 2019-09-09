43120a5341fda72459af65bb2a434132faed5cf7
freddie <freddiebutcher@gmail.com>
2019-09-10 09:36:08 +1000

    created git repo

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.scss';
+import {VideoGrid} from '../../components/VideoGrid/VideoGrid';
+import {SideBar} from '../SideBar/SideBar';
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <SideBar/>
+        <div className='home'>
+            <VideoGrid title='Trending'/>
+            <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/containers/SideBar/SideBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { SideBarItem } from './SideBarItem/SideBarItem';
+import { Menu, Divider } from 'semantic-ui-react';
+import './SideBar.scss';
+import {SideBarHeader} from './SideBarHeader/SideBarHeader';
+import {SideBarFooter} from './SideBarFooter/SideBarFooter';
+
+export class SideBar extends React.Component {
+  render() {
+    return (
+      <Menu borderless vertical stackable fixed='left' className='side-nav'>
+        <SideBarItem highlight={true} label='Home' icon='home' />
+        <SideBarItem label='Trending' icon='fire'/>
+        <SideBarItem label='Followers' icon='spy'/>
+        <Divider/>
+        <SideBarHeader title='Library'/>
+        <SideBarItem label='History' icon='history'/>
+        <SideBarItem label='Watch Later' icon='clock'/>
+        <SideBarItem label='Liked Videos' icon='thumbs up'/>
+        <Divider/>
+        <SideBarHeader title='More from YouTube'/>
+        <SideBarItem label='Movies and Shows' icon='film'/>
+        <Divider/>
+        <SideBarItem label='Report History' icon='flag'/>
+        <SideBarItem label='Help' icon='circle'/>
+        <SideBarItem label='Send Feedback' icon='comment'/>
+        <Divider/>
+        <SideBarFooter/>
+      </Menu>
+    );
+  }
+}
+export default SideBar;
