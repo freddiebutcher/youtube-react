f2c7ca4dbbc8b719ebd0eca31cf5af3a375fae02
freddie <freddiebutcher@gmail.com>
2019-09-10 16:49:33 +1000

    comments

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -4,6 +4,7 @@ import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {Video} from '../../components/Video/Video';
 import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
 import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox';
+import {Comments} from '../Comments/Comments';
 // import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
 
 export class Watch extends React.Component {
@@ -13,7 +14,7 @@ export class Watch extends React.Component {
         <Video className='video' id='-7fuHEEmEjs' />
         <VideoMetadata className='metadata' viewCount={1000}/>
         <VideoInfoBox className='video-info-box'/>
-        <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>Comments</div>
+        <Comments className='comments'/>
         <RelatedVideos className='RelatedVideos'/>
       </div>
     );
